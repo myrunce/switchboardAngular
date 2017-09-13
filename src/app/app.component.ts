f6ac438fdd34aef7916770c5e9274d13495698e2
Myron Navarrete <myronnavarrete@gmail.com>
2017-09-13 15:37:24 -0700

    finished project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  off: boolean = true
+  on: boolean = false
+  temp: boolean;
+  power: string = "Off";
+  switch(index){
+    this.switches[index].temp = this.switches[index].on;
+    this.switches[index].on = this.switches[index].off;
+    this.switches[index].off = this.switches[index].temp;
+    if (this.switches[index].off === true){
+      this.switches[index].power = "Off";
+    }
+    else{
+      this.switches[index].power = "On";
+    }
+  }
+  switches: any[] = [{on: false, off: true, temp: '', power: "Off"}, {on: false, off: true, temp: '', power: "Off"}, {on: false, off: true, temp: '', power: "Off"}, 
+    {on: false, off: true, temp: '', power: "Off"}, {on: false, off: true, temp: '', power: "Off"}, {on: false, off: true, temp: '', power: "Off"}, {on: false, off: true, temp: '', power: "Off"}, 
+    {on: false, off: true, temp: '', power: "Off"}, {on: false, off: true, temp: '', power: "Off"}, {on: false, off: true, temp: '', power: "Off"}];
 }
